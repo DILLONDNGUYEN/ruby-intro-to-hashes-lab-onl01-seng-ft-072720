4ac2018773683c02909a114aabdfb61216d75794
Dillon Nguyen <dillon.d.nguyen@gmail.com>
2020-08-04 06:11:24 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -36,7 +36,12 @@ end
 
 def monopoly_with_third_tier
 	monopoly = {railroads: {
-	  
+	  names: {
+	    reading_railroad:{},
+	    pennsylvania_railroad:{},
+	    b_and_o_railroad:{},
+	    shortline:{}
+	    }
 	  pieces: 4, 
 	  rent_in_dollars: {
 	    one_piece_owned: 25,
@@ -44,12 +49,7 @@ def monopoly_with_third_tier
 	    three_pieces_owned: 100,
 	    four_pieces_owned: 200
 	  }, 
-	  names: {
-	    reading_railroad:{},
-	    pennsylvania_railroad:{},
-	    b_and_o_railroad:{},
-	    shortline:{}
-	    }
+	  
 	  }
 	}
 end
