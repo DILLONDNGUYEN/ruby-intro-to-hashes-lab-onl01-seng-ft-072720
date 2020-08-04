a7abe8c1c0663142760ec8462bbff7545532fafa
Dillon Nguyen <dillon.d.nguyen@gmail.com>
2020-08-04 06:02:25 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -55,7 +55,7 @@ end
 
 #copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
 #1st key of the :reading_railroad hash to a string, 'mortgage_value', whose value is '$100'
-#
+#1st key of the :pennsylvania_railroad hash to a string, 'mortgage_value', whose value is '$200'
 #
 #
 
